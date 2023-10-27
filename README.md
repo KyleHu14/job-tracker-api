1b9a09bb605a0b05fab06d82023ec24cf0d219a7
KyleHu14 <lloydpearce4@gmail.com>
2023-10-26 23:35:45 -0700

    Added some simple routes for the job_app route file

--- README.md
+++ README.md
@@ -3,10 +3,11 @@
 2. Run the command "uvicorn app.main:app"
 
 # Structure of Project
-    app
-    ├── models
+    app                # App : Root Folder
+    ├── models         # Models : Contains the pydantic schemas used in its respective routes file
     │   └── job_app.py
-    ├── routers
-    │   └── job_app.py
+    ├── api            # API : Contains all files API related
+    │   └── routes     # Routes : Contains all API routes that are registered in main.py
+    │       └── job_app.py
     ├── __init__.py
     └── main.py
--- app/api/routes/job_app.py
+++ app/api/routes/job_app.py
@@ -3,7 +3,18 @@ from ...models.job_app import JobApp
 
 router = APIRouter()
 
+# [POST Routes]
 @router.post("/jobapp")
 def create_job_app(new_job_app: JobApp):
-    print(new_job_app)
-    return {"data" : "new posat"}
+    return {"data" : "New Job App!"}
+
+# [GET Routes]
+@router.get("/jobapp")
+def get_job_app():
+    return {"data" : "Current Job Apps"}
+
+# [DELETE Routes]
+@router.delete("/jobapp/{job_id}")
+def delete_job_app(job_id : int):
+    dataStr = "Deleted Job App" + job_id
+    return {"data" : "Deleted Job App"}
