f14678b399c383b5a724bfd54759bb2acbbc8e3b
KyleHu14 <lloydpearce4@gmail.com>
2023-12-20 16:36:39 +0800

    Fixed a bug in utils, now when editing a user, you must put -1 for int fields when you DONT want to change them

--- app/utils.py
+++ app/utils.py
@@ -5,7 +5,9 @@ def convert_to_object(inputClass):
     finalObj = {}
 
     for row in inputClass:
-        if row[1]:
+        if row[1] and isinstance(row[1], str):
+            finalObj[row[0]] = row[1]
+        elif row[1] > 0:
             finalObj[row[0]] = row[1]
 
     return finalObj
