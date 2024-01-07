18f98e5f9684dcdc239d00850a58cde04a13bed1
Lloyd <lloydpearce4@gmail.com>
2024-01-07 12:39:35 -0800

    Deleting a job app now decrements the status and total counts

--- app/api/routes/job_app.py
+++ app/api/routes/job_app.py
@@ -82,6 +82,10 @@ def delete_job_app(job_id : int):
     try:
         res = supabase.table("job_apps").delete().eq("id", job_id).execute()
 
+        # Decrement total apps
+        decrementRes = supabase.rpc("decrement_total_apps", params={"useremail" : res.data[0]["user_email"]}).execute()
+        decrementStatusRes = supabase.rpc(f"decrement_{res.data[0]["status"]}_apps", params={"useremail" : res.data[0]["user_email"]}).execute()
+
         return {"data" : res.data, "error" : "none"}
     # If an Exception has occured
     except Exception as err:
